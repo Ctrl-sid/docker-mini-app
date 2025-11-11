8e801e11a1429134879af87073343bb6f8332a7d
Sidharth S Lal <sidharthslal20@gmail.com>
2025-11-11 21:44:38 +0530

    use render backend URL

--- frontend/my-react-app/src/App.js
+++ frontend/my-react-app/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/hello')
+    fetch('http://https://docker-mini-app-ockf.onrender.com/api/hello')
       .then(res => res.json())
       .then(data => setMessage(data.message));
   }, []);
